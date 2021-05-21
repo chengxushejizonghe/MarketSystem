631a625499a965e7dab701238c042d189fcb6e4b
linjingshu <1279897306@qq.com>
2021-05-21 15:41:20 +0800

    update

--- src/entry/Customer.java
+++ src/entry/Customer.java
@@ -1,5 +1,13 @@
 package entry;
 
-public class Customer {
+import java.util.Date;
 
+public class Customer {
+	String name;
+	String id;
+	int balance;
+	String password;
+	String realName;
+	Date creationTime;
+	
 }
