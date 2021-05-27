d6b25c77ab6f99546a3bd6c66536106c96fd2670
林敬舒 <1279897306@qq.com>
2021-05-27 12:39:35 +0800

    添加Customer注释

--- src/entry/Customer.java
+++ src/entry/Customer.java
@@ -3,11 +3,18 @@ package entry;
 import java.util.Date;
 
 public class Customer {
+	/**用户名*/
 	String username;
+	/**用户编号*/
 	String id;
+	/**用户余额*/
 	int balance;
+	/**用户密码*/
 	String password;
+	/**真实姓名*/
 	String realName;
+	/**注册时间*/
 	Date creationTime;
+	/**联系电话*/
 	String phone;
 }
