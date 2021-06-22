ce134238f2db1f56ee2c3335a56d215d9119b2e2
林敬舒 <1279897306@qq.com>
2021-06-22 17:00:55 +0800

    控制层查找所有商品功能

--- src/controller/ProductController.java
+++ src/controller/ProductController.java
@@ -1,9 +1,19 @@
 package controller;
 
+import entry.Product;
+import service.ProductService;
+import service.impl.ProductServiceImpl;
+
+import java.util.List;
+
 /**
  * @author 林敬舒
  * @create 2021-06-21-17:15
  */
 public class ProductController {
+    ProductService productService = new ProductServiceImpl();
 
+    public List<Product> findProducts(){
+        return productService.findProducts();
+    }
 }
